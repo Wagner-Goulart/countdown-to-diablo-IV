082ca7558e4310f497c8d60c8cee5bdb9eba4f1b
https://github.com/Wagner-Goulart <helga.goulart@jrcomercio.net>
2023-04-23 18:08:19 -0300

    refatorado as funções de JS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,30 +7,43 @@ const timerContainer = document.querySelector('.contagem-regressiva')
 const descricao = document.querySelector('.descricao')
 const spinner = document.querySelector('.spinner')
 
-function contagemRegressiva() {
-    const dataAtual = new Date()
-    const diferenca = dataLancamento-dataAtual;
-    const segundosAteOlancamento = Math.floor(diferenca / 1000) % 60
-    const minutosAteOlancamento = Math.floor(diferenca / 1000 / 60) %60
-    const horasAteOlancamento = Math.floor(diferenca / 1000 /60 /60) %24
-    const diasAteOlancamento = Math.floor(diferenca / 1000 /60 /60/24)
-    
-    segundos.textContent = segundosAteOlancamento < 10 ? "0"+ segundosAteOlancamento : segundosAteOlancamento
-    minutos.innerHTML = minutosAteOlancamento < 10 ? "0"+minutosAteOlancamento : minutosAteOlancamento
-    horas.innerHTML = horasAteOlancamento < 10 ? "0" + horasAteOlancamento : horasAteOlancamento
-    dias.innerHTML = diasAteOlancamento < 10 ? "0" + diasAteOlancamento : diasAteOlancamento
+setTimeout(()=>{
+    adicionarRemoverElementosDaTela ()
+},1000)
 
-}
+setInterval(contagemRegressiva, 1000);
 
-const elementosDeLoading = ()=> {
+ function adicionarRemoverElementosDaTela () {
     spinner.style.display = 'none'
     timerContainer.style.display = 'flex'
     descricao.style.display = 'block'
 }
 
-setTimeout(()=>{
-    elementosDeLoading()
-},1000)
+function contagemRegressiva() {
+    const { segundosAteOlancamento, minutosAteOlancamento, horasAteOlancamento, diasAteOlancamento} = calcularDataAteOLancamento()
 
-setInterval(contagemRegressiva, 1000);
+    segundos.textContent = checarSeMenorQueZero(segundosAteOlancamento)
+    minutos.innerHTML = checarSeMenorQueZero(minutosAteOlancamento)
+    horas.innerHTML =checarSeMenorQueZero(horasAteOlancamento)
+    dias.innerHTML = checarSeMenorQueZero(diasAteOlancamento)
+
+}
+
+function checarSeMenorQueZero(unidadeDeTempo) {
+    return unidadeDeTempo < 10 ? "0" + unidadeDeTempo : unidadeDeTempo
+}
+
+function calcularDataAteOLancamento() {
+    const dataAtual = new Date()
+    const diferenca = dataLancamento - dataAtual
+    const segundosAteOlancamento = Math.floor(diferenca / 1000) % 60
+    const minutosAteOlancamento = Math.floor(diferenca / 1000 / 60) % 60
+    const horasAteOlancamento = Math.floor(diferenca / 1000 / 60 / 60) % 24
+    const diasAteOlancamento = Math.floor(diferenca / 1000 / 60 / 60 / 24)
+
+    return {segundosAteOlancamento, minutosAteOlancamento, horasAteOlancamento, diasAteOlancamento}
+}
+
+
+
 
